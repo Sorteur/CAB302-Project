4333ed2406cd372c697b4ef2152ddd810d35d867
Tyler Ellwood <n10750223@qut.edu.au>
2022-03-29 11:20:05 +1000

    Test 2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,2 @@
-import javax.swing.*;
-
 public class Main {
-    public static void main (String[] args){
-
-        Maze testmaze = new Maze(100,70);
-        SwingUtilities.invokeLater(new GUI2(testmaze));
-
-        //testmaze.GridInfo();
-        //testmaze.Search(1,16);
-    }
 }
--- src/main.java
+++
@@ -1,7 +0,0 @@
-import java.sql.Array;
-
-public class main {
-    public static void Main (String[] args){
-        //new Cell() {};
-    }
-}
