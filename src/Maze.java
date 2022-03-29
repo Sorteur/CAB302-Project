fdcfb9ab8792728e673aa7406d80cc8a9da62a0c
Tyler Ellwood <Tyler7cool@gmail.com>
2022-03-29 22:19:36 +1000

    Just Testing what we could do to generate cells for the mazes, currently just a grid no I/O.

--- src/Maze.java
+++ src/Maze.java
@@ -1,27 +1,31 @@
-//import java.awt.*;
+
 import java.util.*;
 
 public class Maze {
     int Length;
     int Height;
     int Size;
-
+    ArrayList<Cell> Grid = new ArrayList<>();
     public Maze(int length, int height) {
         Length = length;
         Height = height;
         Size = length*height;
         int l = length;
-        List Cells = new ArrayList<Cell>();
         while (l > 0){
             int h=height;
             while (h > 0){
-                Cells.add(new Cell(l,h,true,true,true,true,false,false));
-                System.out.println("New Cell Created with coords"+l+h);
+                Grid.add(new Cell(l-1,h-1,true,true,true,true,false,false));
                 h-=1;
             }
             l-=1;
         }
-        //new Cell(length,height,true,true,true,true,false,false);
+    }
+
+    public void GridInfo(){
+        for (Cell cells:Grid){
+            System.out.println("This Cell's Position is "+cells.getPosX()+","+cells.getPosy());
+        }
+
     }
 
 
