f76b950b9f44d68f9ae5f2fd789ac4de53267b91
Tyler Ellwood <n10750223@qut.edu.au>
2022-03-29 11:20:05 +1000

    Test 2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,2 @@
-import UserInterface.GUI2;
-import javax.swing.*;
-
 public class Main {
-    public static void main (String[] args){
-
-
-
-        SwingUtilities.invokeLater(new GUI2());
-    }
 }
