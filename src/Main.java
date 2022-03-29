6f17d98332b0fc6538b5c754966ff0c2fe6564bd
Tyler Ellwood <Tyler7cool@gmail.com>
2022-03-29 22:19:36 +1000

    Just Testing what we could do to generate cells for the mazes, currently just a grid no I/O.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main (String[] args){
-        Maze testmaze = new Maze(2,3);
-
+        Maze testmaze = new Maze(4,6);
+        testmaze.GridInfo();
     }
 }
