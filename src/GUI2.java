3fbcb7afe08567bb67ea6ab59f7d1afa8b00d4d9
Tyler Ellwood <n10750223@qut.edu.au>
2022-04-05 11:47:03 +1000

    Test 5

--- src/GUI2.java
+++ src/GUI2.java
@@ -1,97 +1,63 @@
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 
-public class GUI2 implements ActionListener, Runnable, ComponentListener {
+public class GUI2 extends JFrame implements ActionListener, Runnable {
     public static final int WIDTH = 500;
     public static final int HEIGHT = 500;
-    public GUI2(Maze maze) throws HeadlessException {
-    currentmaze = maze;
-    }
+    public Maze CurrentMaze = new Maze(6,4);
+    public GUI2(String string) throws HeadlessException {
 
-    Maze currentmaze;
+    }
 
     private void createGUI() {
-        JFrame frame = new JFrame();
-        frame.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
-        frame.setSize(WIDTH, HEIGHT);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        frame.setLayout(new BorderLayout());
-
-        JMenuBar mnuBar = new JMenuBar();
-        frame.add(mnuBar, BorderLayout.NORTH);
-        mnuBar.setPreferredSize(new Dimension(1, 20));
-
-        pnlMaze = createPanel(Color.WHITE);
-        pnlMaze.addComponentListener(this);
-
-        pnlMaze.setLayout(new BorderLayout());
-        frame.add(pnlMaze, BorderLayout.CENTER);
-
-        pnlLeft = createPanel(Color.LIGHT_GRAY);
-        frame.add(pnlLeft, BorderLayout.WEST);
-        pnlLeft.setPreferredSize(new Dimension(100, 1));
-
-        //frame.pack();
-
-        DrawGrid MazeGrid = new DrawGrid(pnlMaze,5,currentmaze.Length,currentmaze.Height);
-        //System.out.println(pnlMaze.getSize());
-        //System.out.println(pnlMaze.getY());
-
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        setVisible(true);
+        pnlOne = createPanel(Color.white);
+        //pnlOne.add(m);
+        pnlTwo = createPanel(Color.GRAY);
+        pnlThree = createPanel(Color.gray);
+        getContentPane().add(pnlOne,BorderLayout.CENTER);
+        getContentPane().add(pnlTwo,BorderLayout.WEST);
+        getContentPane().add(pnlThree,BorderLayout.NORTH);
+        LayoutPanel();
     }
 
-    private JPanel pnlMaze;
-    private JPanel pnlLeft;
-
+    private JPanel pnlOne;
+    private JPanel pnlTwo;
+    private JPanel pnlThree;
 
     private JPanel createPanel(Color c) {
         JPanel A = new JPanel();
         A.setBackground(c);
         return A;
     }
-
-
-
-
-
-
-
-
-
+    //DrawGrid m = new DrawGrid(1,CurrentMaze.Length,CurrentMaze.Height);
+    private void LayoutPanel(){
+        GridBagLayout layout = new GridBagLayout();
+        //add components to grid
+        GridBagConstraints constraints = new GridBagConstraints();
+//Defaults
+        constraints.fill = GridBagConstraints.NONE;
+        constraints.anchor = GridBagConstraints.CENTER;
+        constraints.weightx = 100;
+        constraints.weighty = 100;
+        //pnlOne.setLayout(layout);
+        //pnlTwo.setLayout(layout);
+        //pnlThree.setLayout(layout);
+        //pnlOne.add(m);
+    }
 
     @Override
     public void run(){
         createGUI();
-
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
-    }
-
-    @Override
-    public void componentResized(ComponentEvent e) {
-        System.out.println(pnlMaze.getSize());
-    }
-
-    @Override
-    public void componentMoved(ComponentEvent e) {
-
-    }
-
-    @Override
-    public void componentShown(ComponentEvent e) {
-
-    }
-
-    @Override
-    public void componentHidden(ComponentEvent e) {
 
     }
 }
