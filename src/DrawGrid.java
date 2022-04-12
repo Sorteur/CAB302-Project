a0d9240d336c006b3c139cd97cfef73cfd64ffb7
Tyler Ellwood <Tyler7cool@gmail.com>
2022-04-13 09:07:33 +1000

    Added GUI and temp Grid drawing

--- src/DrawGrid.java
+++ src/DrawGrid.java
@@ -1,33 +1,42 @@
+import javax.swing.*;
 import java.awt.*;
 
 public class DrawGrid extends Canvas {
-    public DrawGrid(int Scale, int x, int y) {
+    public DrawGrid(JPanel panel, int Scale, int x, int y) {
         this.scale = Scale;
         this.X = x;
         this.Y = y;
+        panel.add(this, BorderLayout.CENTER);
+        this.panel = panel;
     }
-
+    JPanel panel;
     int scale;
     int X;
     int Y;
 
     public void paint(Graphics g) {
-        int i = X;
+        int i = 0;
         int size;
         if (scale == 1){
             size = 64;
         } else {
             size = 16;
         }
-        while (i > 0){
-            int j=Y;
-            while (j > 0){
-                g.drawRect(20+(i*size),20+(j*size),size,size);
-                j-=1;
+
+
+
+
+        /*
+        while (i < X){
+            int j=0;
+            while (j < Y){
+                g.drawRect(((panel.getWidth()-(size*X))/2)+(i*size),((panel.getHeight()-(size*Y))/2)+(j*size),size,size);
+                j+=1;
             }
-            i-=1;
+            i+=1;
         }
+        */
+
 
     }
-
 }
