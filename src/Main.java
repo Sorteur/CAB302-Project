c0e9fd8ca58d04170649a6d39ca28966e87c3813
Tyler Ellwood <Tyler7cool@gmail.com>
2022-04-13 09:07:33 +1000

    Added GUI and temp Grid drawing

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import javax.swing.*;
 public class Main {
     public static void main (String[] args){
 
-        Maze testmaze = new Maze(6,3);
-        SwingUtilities.invokeLater(new GUI2("BorderLayout"));
+        Maze testmaze = new Maze(100,70);
+        SwingUtilities.invokeLater(new GUI2(testmaze));
         //GUI.RunGUI(testmaze);
 
 
