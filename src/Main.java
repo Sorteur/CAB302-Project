527f01b50a7a20fd55ed1a6c8cb6668cba5a8dbc
Tyler Ellwood <Tyler7cool@gmail.com>
2022-04-14 14:19:10 +1000

    Added Button functionallity to enable and disable Walls on cells

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class Main {
     public static void main (String[] args){
 
-        Maze currentMaze = new Maze(2,2);
+        Maze currentMaze = new Maze(3,3);
         SwingUtilities.invokeLater(new GUI2(currentMaze));
 
 
