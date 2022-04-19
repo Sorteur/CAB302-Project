4d9ac0dba64c8199c49d178c1f53e6bb6852e527
Tyler Ellwood <n10750223@qut.edu.au>
2022-04-19 14:36:05 +1000

    Test 7

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class Main {
     public static void main (String[] args){
 
-        Maze currentMaze = new Maze(100 ,100);
+        Maze currentMaze = new Maze(6 ,4);
         SwingUtilities.invokeLater(new GUI2(currentMaze));
 
 
