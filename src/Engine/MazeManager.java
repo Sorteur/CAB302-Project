1bc4e20ab2a026ca3ed4ef6b17f3f6b94cf00db0
Zachary Higgs <zac.h@futurerevolutions.com>
2022-04-28 12:32:23 +1000

    Created MazeManager Singleton

--- src/Engine/MazeManager.java
+++ src/Engine/MazeManager.java
@@ -1,4 +1,16 @@
 package Engine;
 
 public class MazeManager {
+    private static MazeManager instance = new MazeManager();
+    private MazeManager (){
+
+    }
+
+    public static MazeManager Instance(){
+        return instance;
+    }
+
+    public void writesomething () {
+        System.out.println("Something");
+    }
 }
