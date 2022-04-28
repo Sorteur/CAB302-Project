d9923381c25dcedc6f891526cc53199841975737
Tyler <tyler7cooL@gmail.com>
2022-04-28 13:09:15 +1000

    Added Start-GUI

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,9 @@ import javax.swing.*;
 public class Main {
     public static void main (String[] args){
 
-        Maze currentMaze = new Maze(6 ,4);
-        SwingUtilities.invokeLater(new GUI2(currentMaze));
-        MazeManager.Instance().CreateMaze(4,5);
+        //Maze currentMaze = new Maze(6 ,4);
+        SwingUtilities.invokeLater(new GUI2());
+       // MazeManager.Instance().CreateMaze(4,5);
 
     }
 }
