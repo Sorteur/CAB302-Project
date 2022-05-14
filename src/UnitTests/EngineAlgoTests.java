dbc9b7248cd77a809e3010a978da1dc5b0881602
Zachary Higgs <zac.h@futurerevolutions.com>
2022-05-14 11:10:06 +1000

    Added imports for Unit test with reference examples from week 8

--- src/UnitTests/EngineAlgoTests.java
+++ src/UnitTests/EngineAlgoTests.java
@@ -1,4 +1,24 @@
 package UnitTests;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
 public class EngineAlgoTests {
+    @BeforeEach @Test
+    public void setUpMovieList() {
+        movies = new MovieList();
+    }
+
+    /* Test 2: Adding a new movie to the list
+     * [This test obliges you to add methods called "addMovie"
+     * and "getRating".  (For now, only getRating needs to
+     * so anything, and all it needs to do is return the
+     * string "No rating".)]
+     */
+    @Test
+    public void addAMovie() throws MovieListException {
+        movies.addMovie("Star Wars");
+        assertEquals("No rating", movies.getRating("Star Wars"),
+                "Adding movie failed");
+    }
 }
