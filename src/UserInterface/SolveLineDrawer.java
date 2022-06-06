63015391937e0ca742fb8bfd31edbf9229659745
Sorteur <Tyler7cooL@gmail.com>
2022-06-07 09:36:50 +1000

    Started Unit-Testing

---
+++ src/UserInterface/SolveLineDrawer.java
@@ -0,0 +1,34 @@
+package UserInterface;
+
+import Engine.MazeManager;
+import Engine.MazeManager;
+import javax.swing.*;
+import java.awt.*;
+
+
+public class SolveLineDrawer extends JPanel{
+    public SolveLineDrawer() {
+        this.setOpaque(false);
+    }
+    public void paintComponent(Graphics g){
+
+        if(MazeManager.Instance().GetMaze().isImgSrtEnd()){
+
+            int Endx = MazeManager.Instance().GetMaze().Search(MazeManager.Instance().GetMaze().getExitImage().GetPositionX(),MazeManager.Instance().GetMaze().getExitImage().GetPositionY()).GetPosX();
+            int Endy = MazeManager.Instance().GetMaze().Search(MazeManager.Instance().GetMaze().getExitImage().GetPositionX(),MazeManager.Instance().GetMaze().getExitImage().GetPositionY()).GetPosY();
+
+
+            super.paintComponent(g);
+            g.drawImage(MazeManager.Instance().GetMaze().getEntryImage().GetImage(),
+                    MazeManager.Instance().GetMaze().Search(0,0).GetPosX()+1,
+                    MazeManager.Instance().GetMaze().Search(0,0).GetPosY()+1,
+                    this);
+
+            g.drawImage(MazeManager.Instance().GetMaze().getExitImage().GetImage(),
+                    Endx+1,
+                    Endy+1,
+                    this);
+        }
+    }
+
+}
