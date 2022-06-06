40f401b182ff9aea04bcca7173bf207c4f3be419
Sorteur <Tyler7cooL@gmail.com>
2022-06-06 10:45:14 +1000

    Bug Fixing

--- src/UserInterface/SrtEndPlacer.java
+++ src/UserInterface/SrtEndPlacer.java
@@ -10,19 +10,22 @@ public class SrtEndPlacer extends JPanel{
     }
     public void paintComponent(Graphics g){
 
-        int Endx = MazeManager.Instance().GetMaze().Search(MazeManager.Instance().GetMaze().getExitImage().GetPositionX(),MazeManager.Instance().GetMaze().getExitImage().GetPositionY()).GetPosX();
-        int Endy = MazeManager.Instance().GetMaze().Search(MazeManager.Instance().GetMaze().getExitImage().GetPositionX(),MazeManager.Instance().GetMaze().getExitImage().GetPositionY()).GetPosY();
+        if(MazeManager.Instance().GetMaze().isImgSrtEnd()){
+
+            int Endx = MazeManager.Instance().GetMaze().Search(MazeManager.Instance().GetMaze().getExitImage().GetPositionX(),MazeManager.Instance().GetMaze().getExitImage().GetPositionY()).GetPosX();
+            int Endy = MazeManager.Instance().GetMaze().Search(MazeManager.Instance().GetMaze().getExitImage().GetPositionX(),MazeManager.Instance().GetMaze().getExitImage().GetPositionY()).GetPosY();
 
 
-        super.paintComponent(g);
-        g.drawImage(MazeManager.Instance().GetMaze().getEntryImage().GetImage(),
-                MazeManager.Instance().GetMaze().Search(0,0).GetPosX()+1,
-                MazeManager.Instance().GetMaze().Search(0,0).GetPosY()+1,
-                this);
+            super.paintComponent(g);
+            g.drawImage(MazeManager.Instance().GetMaze().getEntryImage().GetImage(),
+                    MazeManager.Instance().GetMaze().Search(0,0).GetPosX()+1,
+                    MazeManager.Instance().GetMaze().Search(0,0).GetPosY()+1,
+                    this);
 
-        g.drawImage(MazeManager.Instance().GetMaze().getExitImage().GetImage(),
-                Endx+1,
-                Endy+1,
-                this);
+            g.drawImage(MazeManager.Instance().GetMaze().getExitImage().GetImage(),
+                    Endx+1,
+                    Endy+1,
+                    this);
+        }
     }
 }
