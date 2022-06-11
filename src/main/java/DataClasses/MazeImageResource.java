cf62609387de49b066239405fb34cb43f978889c
Zachary Higgs <zac.h@futurerevolutions.com>
2022-06-11 21:59:15 +1000

    MazeImageResource JavaDocs

--- src/main/java/DataClasses/MazeImageResource.java
+++ src/main/java/DataClasses/MazeImageResource.java
@@ -7,13 +7,34 @@ public class MazeImageResource
     private final int PositionX;
     private final int PositionY;
 
+    /**
+     * A wrapper containing the components to place an Image on a Maze
+     * Includes Image, X position, Y position.
+     * @param  image An Image to be placed
+     * @param  positionX An int as the X coordinate on the Maze
+     * @param  positionY An int as the Y coordinate on the MAze
+     * @see Maze
+     */
     public MazeImageResource(Image image, int positionX, int positionY){
         Image = image;
         PositionX = positionX;
         PositionY = positionY;
     }
-
+    /**
+     * Get an Image in the wrapper
+     * @return Image an Image
+     */
     public Image GetImage(){ return Image; }
+    /**
+     * Get the Images X position in the Maze
+     * @return int value for X position of Image
+     * @see Maze
+     */
     public int GetPositionX(){ return PositionX; }
+    /**
+     * Get the Images Y position in the Maze
+     * @return int value for Y position of Image
+     * @see Maze
+     */
     public int GetPositionY() { return PositionY; }
 }
