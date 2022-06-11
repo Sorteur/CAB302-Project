3ac6b0da909dbf25e71b3233174ee22e04bb91dd
Zachary Higgs <zac.h@futurerevolutions.com>
2022-06-11 12:59:54 +1000

    DBConnection Unit Tests

---
+++ src/test/java/DBConnectionTest.java
@@ -0,0 +1,33 @@
+import DataModules.DBConnection;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+public class DBConnectionTest {
+
+    DBConnection connection;
+    @BeforeEach
+
+    @Test
+    void Test(){
+        connection = DBConnection.Instance();
+    }
+
+    @Test
+    void Test2() throws SQLException{
+        connection.StartTransaction();
+    }
+
+    @Test
+    void Test3() throws SQLException{
+        connection.CommitTransaction();
+    }
+
+    @Test
+    void Test4() throws SQLException{
+        connection.RollbackTransaction();
+    }
+
+
+}
